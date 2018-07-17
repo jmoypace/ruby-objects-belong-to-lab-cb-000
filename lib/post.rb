b7688ab8a5b30c5b636247a728ae05529e515140
jmoypace <jmoypace@gmail.com>
2018-07-17 01:49:36 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,19 +1,4 @@
 class Post
-attr_accessor :title
+attr_accessor :title, :author
 
-end
-
-
-  it "belongs to an author" do
-    author = Author.new
-    post.author = author
-    expect(post.author).to eq(author)
-  end
-
-  it "knows the name of the author it belongs to" do
-    author = Author.new
-    author.name = "Uncle Bob"
-    post.author = author
-    expect(post.author.name).to eq("Uncle Bob")
-  end
 end
